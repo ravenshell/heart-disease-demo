83d13ae054db1382507edaa66d339a5e809260d4
Ogie Ben <bensoft2k5@gmail.com>
2018-07-24 21:03:29 +0100

    made keras model functional

--- data.py
+++ data.py
@@ -65,3 +65,60 @@ def get_array_values(features, labels):
 # print(data_url())
 # print(prepare_data())
 # print(make_data_tensors(features, labels, 32))
+
+#  -------------------------------Using tf.data ---------------------------------------------------
+
+
+
+# data preparation
+
+# these should be added to constructor
+input = 13
+#field_type
+
+def prepare_data(data_file):
+   # instantiate TextLineDataset(data_file) that will
+   # be used to retrive content of data_file
+   training_data = tf.data.TextLineDataset(data_file)
+   #training_data = training_data.skip(1)
+
+   # apply parse_csv_data() function to the data
+   training_data = training_data.map(parse_csv_data)
+
+   # random shuffle
+   training_data = training_data.shuffle(2000)
+   # batch size must greater than 30
+   # to attain the normal distribution
+   training_data = training_data.batch(50)
+
+
+   return training_data
+
+
+# def getDataset():
+#    # url to file
+#    data_url = "https://archive.ics.uci.edu/ml/machine-learning-databases/heart-disease/processed.cleveland.data"
+#    #data_url = "http://download.tensorflow.org/data/iris_training.csv"
+#    # return file path of the data
+#    return tf.keras.utils.get_file(os.path.basename(data_url), origin=data_url)
+#
+
+def parse_csv_data(csv_current_line):
+   # set the field type of each entry from our csv dataset
+   #fields_type = [[0.], [0.], [0.], [0.], [0]]
+   fields_type = [[0.], [0.], [0.], [0.], [0.], [0.], [0.], [0.], [0.], [0.], [0.], [0.], [0.], [0]]
+
+   # decode csv using tf.decode_csv() function
+   data = tf.decode_csv(csv_current_line, fields_type)
+
+   # seperate fetures from ouput
+
+   # strip the first 13 features into a single tensor
+   # print(data[:-1])
+   features = tf.reshape(data[:-1], shape=(input,))
+   # print(features)
+
+   # strip the last feature as our label
+   labels = tf.reshape(data[-1], shape=())
+
+   return features, labels
